b069dc0a6b0ed20df01b62cae43397ba4306df04
Mark Burch <bemark@gmail.com>
2026-06-22 11:51:57 +1000

    Use gray-matter instead of hand rolled metadata parser

--- components/mdx/utils.ts
+++ components/mdx/utils.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import path from "path";
+import matter from "gray-matter";
 
 type Metadata = {
   title: string;
@@ -11,31 +12,14 @@ type Metadata = {
   authorImg?: string;
 };
 
-function parseFrontmatter(fileContent: string) {
-  const frontmatterRegex = /---\s*([\s\S]*?)\s*---/;
-  const match = frontmatterRegex.exec(fileContent);
-  const frontMatterBlock = match![1];
-  const content = fileContent.replace(frontmatterRegex, "").trim();
-  const frontMatterLines = frontMatterBlock.trim().split("\n");
-  const metadata: Partial<Metadata> = {};
-
-  frontMatterLines.forEach((line) => {
-    const [key, ...valueArr] = line.split(": ");
-    let value = valueArr.join(": ").trim();
-    value = value.replace(/^['"](.*)['"]$/, "$1"); // Remove quotes
-    metadata[key.trim() as keyof Metadata] = value;
-  });
-
-  return { metadata: metadata as Metadata, content };
-}
-
 function getMDXFiles(dir: string) {
   return fs.readdirSync(dir).filter((file) => path.extname(file) === ".mdx");
 }
 
 function readMDXFile(filePath: string) {
   const rawContent = fs.readFileSync(filePath, "utf-8");
-  return parseFrontmatter(rawContent);
+  const { data, content } = matter(rawContent);
+  return { metadata: data as Metadata, content: content.trim() };
 }
 
 function getMDXData(dir: string) {

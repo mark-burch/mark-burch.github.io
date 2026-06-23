8d9f457f8ccd41e29e4e04acb8aa5e4f9c814749
Mark Burch <bemark@gmail.com>
2026-06-23 22:25:00 +1000

    Added the other two podcasts

--- app/resources/podcasts.tsx
+++ app/resources/podcasts.tsx
@@ -7,7 +7,9 @@ export const PODCASTS = [
       <>
         Hosted by computer-science professor Cal Newport, the podcast presents —
         in my opinion — one of the most balanced and nuanced takes on AI and
-        digital ethics that I've seen.
+        digital ethics across the board. It also doesn't hurt that his academic
+        background means he can tread deeper into the subject matter without
+        sounding clueless.
       </>
     ),
     extended: (
@@ -17,7 +19,7 @@ export const PODCASTS = [
         financial perspective. You won't find hype (or anti-hype) here, only
         objective and evidence-based discussions of the tech. One of the most
         important ideas that Cal is popularising at the moment is that the deep
-        negative psychological impact of AI doomerism hype far outweighs the
+        negative psychological impact of AI "doomerism" hype far outweighs the
         merits of the technology.
       </>
     ),
@@ -46,6 +48,82 @@ export const PODCASTS = [
           target="_blank"
         >
           Do I need a brain gym?
+        </a>
+      </>
+    ),
+  },
+  {
+    id: 1,
+    image: "/images/recommendations/podcasts/tech-wont-save-us.png",
+    title: "Tech Won't Save Us",
+    summary: (
+      <>
+        Award winning left-wing tech podcast hosted by Canadian technology
+        writer Paris Marx. Each episode has a guest or two on to discuss tech
+        news stories that mainstream media outlets are unlikely to pick up and
+        provides a different perspective on an industry that has so much
+        influence over our lives.
+      </>
+    ),
+    extended: (
+      <>
+        The podcast acts as an excellent critique of the tech industry but
+        sometimes has a habit of veering into politics which Australian or
+        non-US listeners might find less interesting. Favourite episodes are
+        about the human impact of tech (LLMs in the classroom, anyone?).
+      </>
+    ),
+    footer: (
+      <>
+        Favourite episodes:{" "}
+        <a
+          className="font-medium text-sky-500 hover:underline"
+          href="https://www.techwontsave.us/episode/333_canadas_government_is_rushing_ai_adoption_w_hadrian_mertins_kirkwood"
+          target="_blank"
+        >
+          Canada’s Government is Rushing AI Adoption
+        </a>
+        ,{" "}
+        <a
+          className="font-medium text-sky-500 hover:underline"
+          href="https://www.techwontsave.us/episode/325_the_problem_with_ceo_said_a_thing_journalism_w_karl_bode"
+          target="_blank"
+        >
+          The Problem with "CEO Said a Thing" Journalism
+        </a>
+      </>
+    ),
+  },
+  {
+    id: 2,
+    image: "/images/recommendations/podcasts/hard-fork.png",
+    title: "Hard Fork",
+    summary: (
+      <>
+        NY Times journalists Kevin Roose and Casey Newton host a podcast that's
+        a little bit more upbeat about AI. Verging on too much hype for one to
+        take, it manages to still be thoroughly entertaining and usually hosts
+        some good conversations with industry leaders.
+      </>
+    ),
+    extended: <></>,
+    footer: (
+      <>
+        Favourite episodes:{" "}
+        <a
+          className="font-medium text-sky-500 hover:underline"
+          href="https://www.nytimes.com/2026/04/24/podcasts/hardfork-tim-cook-yang-ubi.html"
+          target="_blank"
+        >
+          Tim Cook's Legacy
+        </a>
+        ,{" "}
+        <a
+          className="font-medium text-sky-500 hover:underline"
+          href="https://www.nytimes.com/2025/12/12/podcasts/hardfork-australia-water.html"
+          target="_blank"
+        >
+          Australia Kicks Kids Off Social Media
         </a>
       </>
     ),
